0e08348f714fb8843ee138cb21a70ffa9fc5fc70
Haidar Al-Ogaily <haidar@wemakeapps.net>
2020-07-03 18:15:04 +1000

    Initial Commit

---
+++ HSB Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HSB Demo
+//
+//  Created by Haidar Mohammed on 3/7/20.
+//  Copyright © 2020 Haidar AlOgaily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
